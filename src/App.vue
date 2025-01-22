5b74194793ac7446d923ed97b2a2c70a4457c023
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2025-01-22 14:12:54 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <h1>{{ title }}</h1>
+          <img alt="Vue logo" src="./assets/logo.png">
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      title: 'Vue cli'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
