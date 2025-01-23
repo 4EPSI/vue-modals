48956815abd7c4dfbbf6a230dcf44f39112dd62e
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2025-01-23 18:56:30 +0200

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,70 @@
 <template>
   <div class="wrapper">
     <div class="wrapper-content">
+
       <section>
         <div class="container">
-          <h1>{{ title }}</h1>
-          <img alt="Vue logo" src="./assets/logo.png">
+          <button @click="modalFirst = !modalFirst" class="btn btnPrimary">Show first modal</button>
+
+          <modals 
+            title="first modal"
+            v-show="modalFirst"
+            @close="modalFirst = false">
+            <template v-slot:body>
+              <p>text</p>
+              <button @click="modalFirst = !modalFirst" class="btn btnPrimary">Well Done!</button>
+            </template>
+          </modals>
+
+          <button @click="modalSecond.show = !modalSecond.show" class="btn btnPrimary">Show modal with form</button>
+          <modals 
+            title="Modal with form"
+            v-show="modalSecond.show"
+            @close="modalSecond.show = false">
+            <template v-slot:body>
+              <form @submit.prevent="submitSecondForm">
+                <label>Name:</label>
+                <input required type="text" v-model="modalSecond.name">
+                <label>Email:</label>
+                <input required type="email" v-model="modalSecond.email">
+                <button class="btn btnPrimary">Submit!</button>
+              </form>
+            </template>
+          </modals>
         </div>
       </section>
+
     </div>
   </div>
 </template>
 
 <script>
-
+import modals from '@/components/Modal.vue'
 export default {
+  components: {
+    modals
+  },
   data() {
     return {
-      title: 'Vue cli'
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    submitSecondForm() {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email
+      })
+      this.modalSecond.name = ''
+      this.modalSecond.email = ''
+      this.modalSecond.show = false
     }
   }
 }
 </script>
 
-<style>
-
-</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,107 @@
+<template>
+  <transition name="modal">
+    <div @click="$emit('close')" class="modal__wrapper">
+      <div @click.stop="" class="modal-content">
+        <div class="modal-header">
+          <span class="modal-title"> {{ title }} </span>
+          <span @click="$emit('close')" class="button-close">×</span>
+        </div>
+        <div class="modal-body">
+          <slot name="body">default body</slot>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  mounted() {
+    document.body.addEventListener('keyup', event => {
+      if(event.keyCode === 27) this.$emit('close')
+    })
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+// Animation
+// Transition classes
+// .modal-enter-active, .modal-leave-active {
+//   opacity: 1;
+//   transition: opacity 0.2s ease, transform 0.2s ease;
+// }
+
+// .modal-enter-from, .modal-leave-to {
+//   opacity: 0;
+//   transform: scale(1.2);
+// }
+
+// .modal-leave-active .modal-content {
+//   transform: scale(1);
+// }
+
+.modal-enter, .modal-leave-active {
+  opacity: 0
+}
+
+.modal-enter .modal-content,
+.modal-leave-active .modal-content {
+  transform: scale(1.2)
+}
+
+.modal__wrapper{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  transition: opacity .2s ease;
+  right: 0;
+  z-index: 998;
+  background-color: rgba(00,00,00,.48);
+}
+
+.modal-content {
+  position: relative;
+  max-width: 600px;
+  padding: 20px 18px;
+  background-color: #fff;
+  border: 1px solid #dcdfe6;
+  transition: all .2s ease;
+  border-radius: 8px;
+  z-index: 999;
+  overflow: hidden;
+  @media screen and (min-width: 900px) {
+    min-width: 500px;
+  }
+}
+.modal-header {
+  display: flex;
+  align-self: center;
+  justify-content: space-between;
+  padding-bottom: 20px;
+  span {
+    font-size: 24px;
+  }
+  .button-close {
+    cursor: pointer;
+  }
+}
+.modal-body {
+  text-align: center;
+}
+
+
+
+</style>
